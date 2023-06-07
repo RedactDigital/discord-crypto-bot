3e67a86da9b2637060206efff6a1fd17a25d29e3
Patrick Rizzardi <patrick.rizzardi@gmail.com>
2023-06-07 16:57:28 -0400

    update docker stack to be better

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const Discord = require('./services/discord');
 
-console.log(`${process.env.SYMBOL.toUpperCase()}_TOKEN`);
-console.log(`${process.env.SYMBOL.toLocaleUpperCase()}_BOT_ID`);
+const botIdEnv = `${process.env.SYMBOL.toLocaleUpperCase()}_BOT_ID`;
+const tokenEnv = `${process.env.SYMBOL.toUpperCase()}_TOKEN`;
+
+console.log(process.env[botIdEnv]);
+console.log(process.env[tokenEnv]);
 
 // eslint-disable-next-line no-new
 new Discord();
