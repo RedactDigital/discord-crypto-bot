78af122b5ff2d5138e53d977b90f259176ba966d
Patrick Rizzardi <patrick.rizzardi@gmail.com>
2023-06-07 17:06:35 -0400

    update docker stack to be better

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Discord = require('./services/discord');
 
-const botIdEnv = `${process.env.SYMBOL.toLocaleUpperCase()}_BOT_ID`;
-const tokenEnv = `${process.env.SYMBOL.toUpperCase()}BOT_TOKEN`;
+const botIdEnv = `${process.env.SYMBOL.toUpperCase()}_BOT_ID`;
+const tokenEnv = `${process.env.SYMBOL.toUpperCase()}_BOT_TOKEN`;
 
 console.log(process.env[botIdEnv]);
 console.log(process.env[tokenEnv]);
